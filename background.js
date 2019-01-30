eeeadec11d79ebc1b5a824a918607792c3a6da98
Andrew Fischer <afischer15@mac.com>
2019-01-30 12:10:34 -0500

    Refactor, deal with awful HTTP/js redirects for DOI resolution

--- background.js
+++ background.js
@@ -6,8 +6,18 @@ function titleCase(str) {
   return str.join(' ');
 }
 
+function getJournalProp(head, query) {
+  // shakes fist at javascript
+  const prop = head.querySelector(`meta[name='citation_${query}']`)
+               || head.querySelector(`meta[name='dc.${query}']`)
+               || head.querySelector(`meta[name='dc.${titleCase(Query)}']`)
+               || head.querySelector(`meta[name='dc.publisher']`)
+  if (!prop) return;
+  return prop.content
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+  console.log('GOT MESSAGE WITH URL ', request.url);
   const citeProps = ['title']
   const metaProps = {url: request.url}
 
@@ -17,9 +27,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     console.log('Requesting', request.url)
     const headElt = this.responseXML.head
 
-    // get journal title
-
-    // shakes fist at javascript
+    // get journal title, requires a few checks
     metaProps['journal_title'] = headElt.querySelector(`meta[name='citation_journal_title']`) ||
                                  headElt.querySelector(`meta[name='dc.source']`) ||
                                  headElt.querySelector(`meta[name='dc.Source']`) ||
@@ -28,47 +36,24 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     if (!metaProps['journal_title']) {
       // TODO: Deal with horrible DOI javascript redirects
       const nextURL = this.responseXML.body.querySelector('[name="redirectURL"]').value
-      console.warn('I think I need', decodeURI(nextURL))
-
-      console.warn('No Journal found!')
+      console.warn('No journal! Trying with', decodeURIComponent(nextURL))
       console.warn(this.responseXML)
-      console.warn('-------')
-      return
+      xhr.open(request.method, decodeURIComponent(nextURL))
+      return xhr.send()
     }
 
     metaProps['journal_title'] = metaProps['journal_title'].content
 
-
-
-
     // get other props
-    citeProps.forEach((prop) => {
-      metaProps[prop] = headElt.querySelector(`meta[name='citation_${prop}']`) ||
-                        headElt.querySelector(`meta[name='dc.${prop}']`) ||
-                        headElt.querySelector(`meta[name='dc.${titleCase(prop)}']`)
+    citeProps.forEach((prop) => metaProps[prop] = getJournalProp(headElt, prop))
 
-      metaProps[prop] = metaProps[prop].content
-    })
-
-    console.log(metaProps);
-    // callback(this.responseXML.head);
+    console.log('Found properties', metaProps)
     callback(metaProps)
   }
 
-  xhr.open(request.method, request.url);
+  xhr.open(request.method, request.url)
   xhr.responseType = "document";
   xhr.send();
-
-  // console.log(request);
-  // Http.open(request.method, request.url);
-  // Http.responseType = 'document'
-  // Http.send();
-  // Http.onreadystatechange= (response) =>{
-  //   console.log(Http.responseXML);
-  //   if (Http.responseXML && Http.readyState==4 && Http.status==200) {
-  //     callback(Http.responseXML)
-  //   }
-  // }
 
   return true;
 });
