1cfe3148aed5adf96c7a342f00312fb9b4987877
Andrew Fischer <afischer15@mac.com>
2019-07-14 22:42:39 -0400

    Async

--- background.js
+++ background.js
@@ -16,44 +16,69 @@ function getJournalProp(head, query) {
   return prop.content
 }
 
+const getArticleProps = url => {
+  return new Promise((resolve, reject) => {
+    const method = 'GET';
+
+    const citeProps = ['title']
+    const metaProps = {url: url}
+
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.responseType = "document"
+
+    xhr.onload = function() {
+      console.log('Requesting', url)
+      const headElt = this.responseXML.head
+
+      // get journal title, requires a few checks
+      metaProps['journal_title'] = headElt.querySelector(`meta[name='citation_journal_title']`) ||
+                                   headElt.querySelector(`meta[name='dc.source']`) ||
+                                   headElt.querySelector(`meta[name='dc.Source']`) ||
+                                   headElt.querySelector(`meta[name='dc.publisher']`)
+
+      if (!metaProps['journal_title']) {
+        // TODO: better deal with horrible DOI javascript redirects
+        const redirectURL = this.responseXML.body.querySelector('[name="redirectURL"]')
+        const nextURL = redirectURL && redirectURL.value !== null && decodeURIComponent(redirectURL.value)
+        if (!nextURL) return resolve(); // nothing to do
+        console.warn('No journal! Trying with', nextURL)
+        console.warn(this.responseXML)
+        xhr.open(method, decodeURIComponent(nextURL))
+        return xhr.send()
+      }
+
+      metaProps['journal_title'] = metaProps['journal_title'].content
+
+      // get other props
+      citeProps.forEach((prop) => metaProps[prop] = getJournalProp(headElt, prop))
+
+      console.log('Found properties', metaProps)
+      resolve(metaProps)
+    };
+
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+
+    xhr.send()
+  });
+}
+
+function getAllArticleData(articleDataProms, cb) {
+  return Promise.all(articleDataProms).then(articleData => {
+    cb(articleData.filter(data => data != null))
+  }).catch(err => console.err('Got an error', err))
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-  console.log('GOT MESSAGE WITH URL ', request.url)
-  const citeProps = ['title']
-  const metaProps = {url: request.url}
-
-  const xhr = new XMLHttpRequest()
-
-  xhr.onload = function() {
-    console.log('Requesting', request.url)
-    const headElt = this.responseXML.head
-
-    // get journal title, requires a few checks
-    metaProps['journal_title'] = headElt.querySelector(`meta[name='citation_journal_title']`) ||
-                                 headElt.querySelector(`meta[name='dc.source']`) ||
-                                 headElt.querySelector(`meta[name='dc.Source']`) ||
-                                 headElt.querySelector(`meta[name='dc.publisher']`)
-
-    if (!metaProps['journal_title']) {
-      // TODO: Deal with horrible DOI javascript redirects
-      const nextURL = this.responseXML.body.querySelector('[name="redirectURL"]').value
-      console.warn('No journal! Trying with', decodeURIComponent(nextURL))
-      console.warn(this.responseXML)
-      xhr.open(request.method, decodeURIComponent(nextURL))
-      return xhr.send()
-    }
-
-    metaProps['journal_title'] = metaProps['journal_title'].content
-
-    // get other props
-    citeProps.forEach((prop) => metaProps[prop] = getJournalProp(headElt, prop))
-
-    console.log('Found properties', metaProps)
-    callback(metaProps)
-  }
-
-  xhr.open(request.method, request.url)
-  xhr.responseType = "document"
-  xhr.send()
-
-  return true
+  console.log('GOT MESSAGE WITH URLs ', request.urls)
+  const { urls } = request;
+  const articleDataProms = urls.map(url => getArticleProps(url))
+  console.log('data proms', articleDataProms);
+  getAllArticleData(articleDataProms, callback)
+  return true; // async
 })
--- extension.js
+++ extension.js
@@ -1,27 +1,34 @@
-const blacklistRegex = /facebook|fb\.me|twitter|(nyt)(i\.?me?s)?|instagram/g
+const blacklistRegex = /facebook|fb\.me|wikipedia|twitter|(nyt)(i\.?me?s)?|instagram/g
 const articleHed = document.querySelector('article header')
-const articleLinks = document.querySelectorAll('article a[target="_blank"][title]')
+let articleLinks = document.querySelectorAll('article a[target="_blank"][title]')
+articleLinks = [...articleLinks].map(elt => elt.href).filter(url => !url.match(blacklistRegex));
 
 const outerDiv = document.createElement('details')
 outerDiv.className = 'srclist-wrapper'
-outerDiv.innerHTML = '<summary></span><span id="src-count"></span>References</summary>'
+outerDiv.innerHTML = '<summary></span><span id="src-count" class="scitimes-loader"></span>References</summary>'
 articleHed.append(outerDiv);
 
 const sourceDiv = document.createElement('div')
+const srcCount = document.getElementById('src-count')
 sourceDiv.className = 'scitimes-srclist'
 
 outerDiv.appendChild(sourceDiv);
 
-articleLinks.forEach((anchor) => {
-  if (anchor.href.match(blacklistRegex)) return // skip social media, etc
+console.log('links', articleLinks);
 
-  chrome.runtime.sendMessage({
-    method: 'GET',
-    url: anchor.href
-  }, (res) => {
-    if (!res) return
+chrome.runtime.sendMessage({
+  method: 'GET',
+  urls: articleLinks
+}, (res) => {
+  if (!res) return
+
+  // set length div
+  srcCount.classList = ['srclist-number']
+  srcCount.innerHTML = `${res.length}`
+
+  res.forEach(articleData => {
     // destructure properties
-    const {journal_title, title, url} = res
+    const {journal_title, title, url} = articleData
     // create div for each source
     const linkNode = document.createElement('a')
     linkNode.className = 'scitimes-source'
@@ -30,9 +37,30 @@ articleLinks.forEach((anchor) => {
 
     // Append to source list div
     sourceDiv.appendChild(linkNode)
-    document.getElementById('src-count').innerHTML = `${sourceDiv.children.length} `
   })
 })
+
+// articleLinks.forEach((anchor) => {
+//   if (anchor.href.match(blacklistRegex)) return // skip social media, etc
+//
+//   chrome.runtime.sendMessage({
+//     method: 'GET',
+//     url: anchor.href
+//   }, (res) => {
+//     if (!res) return
+//     // destructure properties
+//     const {journal_title, title, url} = res
+//     // create div for each source
+//     const linkNode = document.createElement('a')
+//     linkNode.className = 'scitimes-source'
+//     linkNode.innerHTML = `${journal_title}<div class="src-title">${title}</div>`
+//     linkNode.href = url
+//
+//     // Append to source list div
+//     sourceDiv.appendChild(linkNode)
+//     document.getElementById('src-count').innerHTML = `${sourceDiv.children.length} `
+//   })
+// })
 
 // add source list to article
 // window.onload = () => {
